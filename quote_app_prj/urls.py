507449decfcbd3af951eab12735a7b304353362c
Chris <releasethebadgers@yahoo.co.uk>
2017-12-01 14:47:15 +0000

    Working app

--- quote_app_prj/urls.py
+++ quote_app_prj/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from my_quote_app.views import get_quotes
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
